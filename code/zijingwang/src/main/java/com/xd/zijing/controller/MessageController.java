2ad716ea81545c9dae03a2bdaf75261ea4438fc2
LIBHOLO <870382858@qq.com>
2017-08-28 11:05:41 +0800

    d

--- code/zijingwang/src/main/java/com/xd/zijing/controller/MessageController.java
+++ code/zijingwang/src/main/java/com/xd/zijing/controller/MessageController.java
@@ -82,9 +82,7 @@ public class MessageController {
 		Message message = new Message();
 		List<SenseWord> list = senseService.findAll();
 		SensitivewordFilter filter = new SensitivewordFilter(list);
-		System.out.println("敏感词的数量：" + filter.sensitiveWordMap.size());
 		Set<String> set = filter.getSensitiveWord(messageContent, 1);
-		System.out.println("语句中包含敏感词的个数为：" + set.size() + "。包含：" + set);
 		if(set.size() != 0){
 			req.setAttribute("isSucceed", 0);
 			req.setAttribute("set", set);
